b10a18f4791c67ce196e4ec01e35756b8a748628
abdulsamighl-ui <abdul.sami.ghl@gmail.com>
2026-08-31 08:46:45 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "@leadconnector/vibe-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/portfolio/',
   server: {
     host: "::",
     port: 8080,
